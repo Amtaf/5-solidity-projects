8fd345f0b6975a52417dcd93f8a9f82aa1df923a
Fatma Aq <abdulqadirfatma@gmail.com>
2022-03-12 23:19:10 +0300

    SimpleStorage code

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
 
+pragma solidity >=0.7.0 <0.9.0;
+
+contract SimpleStorage{
+    string public data;
+
+    function setData(string memory _data) public {
+        data = _data;
+    }
+    function fetchData() public view returns(string memory){
+        return data;
+    } 
+}
