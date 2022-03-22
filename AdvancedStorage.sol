fd520e9fd8f61ea0aa5f78114f5fd37f8183fc60
Fatma Aq <abdulqadirfatma@gmail.com>
2022-03-22 23:10:39 +0300

    Update AdvancedStorage.sol

--- AdvancedStorage.sol
+++ AdvancedStorage.sol
@@ -1,1 +1,24 @@
 
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract AdvancedStorage{
+    uint [] public ids;
+
+    function addArr(uint id) public{
+        ids.push(id);
+
+    }
+
+    function getPosition(uint position) public view returns(uint){
+       return ids[position];
+
+    }
+    function getAllArr() public view returns(uint [] memory){
+        return ids;
+    }
+    function length() public view returns(uint) {
+        return ids.length;
+    }
+}
